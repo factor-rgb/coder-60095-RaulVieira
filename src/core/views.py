86fd68088bfaeaf4a9860376d3f1c2599c1034d5
Raul Vieira <raulvieira199@gmail.com>
2024-11-22 09:04:30 -0500

    feat: app core y views

--- src/core/views.py
+++ src/core/views.py
@@ -1,3 +1,20 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def saludar(request):
+	return HttpResponse("Hola desde Django!")
+
+
+def saludar_con_etiqueta():
+      return HttpResponse("<h1> titulo de la app </h1>")
+
+
+def saludar_con_parametros(request, nombre: str, apellido: str):
+    nombre = nombre.capitalize()
+    apellido = apellido.capitalize()
+    return HttpResponse(f"{apellido}, {nombre}")
+
+def index(request):
+      contex = {"año": 2024}
+      return render(request, "core/index.html", contex)
