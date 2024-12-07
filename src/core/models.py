74ac81fc383d5c16704935056ff34590fb535d94
Raul Vieira <raulvieira199@gmail.com>
2024-12-07 09:55:34 -0500

    feat: add 3 models

--- src/core/models.py
+++ src/core/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Categorias_Comidas(models.Model):
+    categoria = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.categoria
+
+
+class Menu(models.Model):
+    platillo = models.CharField(max_length=255, unique=True)
+    categoria_Comida = models.ForeignKey(Categorias_Comidas, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f"{self.platillo} - {self.categoria_Comida}"
+
+
+class Sugerencias_Comidas(models.Model):
+    sugerencia = models.CharField(max_length=255)
+    categoria_comida = models.ForeignKey(Categorias_Comidas, on_delete=models.SET_NULL, null=True)
+
+
+class Reservaciones(models.Model):
+    nombre = models.CharField(max_length=255)
+    reservacion_fecha = models.DateField()
+
+
+class Pedidos(models.Model):
+    nombre = models.CharField(max_length=255)
+    direccion = models.CharField(max_length=255)
+    platillo = models.ForeignKey(Menu, on_delete=models.CASCADE)
