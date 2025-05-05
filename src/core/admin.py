70df09de6e20f3e4230c4736056eafb18b0c5a74
Raul Vieira <raulvieira199@gmail.com>
2025-05-05 07:36:58 -0500

    refactor(menu):menu_list, add 1 super user

--- src/core/admin.py
+++ src/core/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Menu
+from .models import Menu,CategoriasComidas
 
-admin.site.register(Menu)
+# admin.site.register(CategoriasComidas)
+# admin.site.register(Menu)
+
+@admin.register(CategoriasComidas)
+class CategoriasComidasAdmin(admin.ModelAdmin):
+    list_display = ("categoria",)
+    search_fields = ("categoria",)
+    ordering = ("categoria",)
+
+
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ("platillo", "imagen", "categoria_comida", "precio")
+    search_fields = ("platillo", "categoria_comida")
+    ordering = ("platillo", "categoria_comida", "precio")
+    list_filter = ("categoria_comida",)
